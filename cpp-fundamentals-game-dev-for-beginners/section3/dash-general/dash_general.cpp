28bbdf74157618a0ae81ec8cd6502414478e50cc
Athul Muralidhar <athul.muralidhar@print.com>
2024-04-03 14:18:24 +0200

    updated velocity to be frame independent using dT

--- cpp-fundamentals-game-dev-for-beginners/section3/dash-general/dash_general.cpp
+++ cpp-fundamentals-game-dev-for-beginners/section3/dash-general/dash_general.cpp
@@ -9,12 +9,12 @@ int main() {
     // initialise window
     InitWindow(windowWidth, windowHeight, "dasher");
 
-    // game variablescruffyPos.x = windowWidth/2 - scruffyRec.width / 2;
-    int velocity{0};  // pixel per frame
-    const int gravity = 1; // pixel per frame per frame
+    // game variables
+    int velocity{0};  // pixel per second
+    const int gravity = 1000; // pixel per second per second 
     // is the axe in air?
     bool isInAir(false);
-    const int jumpVelocity = -22;
+    const int jumpVelocity = -600; // pixel per second
 
 
     // scruffy
@@ -25,7 +25,7 @@ int main() {
     scruffyRec.x = 0;
     scruffyRec.y = 0;
     Vector2 scruffyPos;
-    // first part alighes the (0,0) of scarfy rec to the centre of the screen, but we need scruffy to be exactly in the centre of the screen so we substract the other bit
+    // first part aligns`   es the (0,0) of scarfy rec to the centre of the screen, but we need scruffy to be exactly in the centre of the screen so we substract the other bit
     scruffyPos.x = windowWidth/2 - scruffyRec.width / 2;
     scruffyPos.y = windowHeight - scruffyRec.height; // this places scruffy on the ground
 
@@ -36,6 +36,9 @@ int main() {
     {
         BeginDrawing();
         ClearBackground(WHITE);
+        
+        // delta time - time since last frame
+        const float dT = GetFrameTime();
 
         // game logic begins
 
@@ -52,7 +55,8 @@ int main() {
         } else {
 
             // rectangle is in the air
-            velocity += gravity; 
+            // velocity = velocity*dT + gravity;
+            velocity += gravity * dT;
             isInAir = true;
         }
         
@@ -60,12 +64,13 @@ int main() {
         if (IsKeyPressed(KEY_SPACE) && !isInAir)
         {
             // make the rectangle jump
+            // velocity = (velocity + jumpVelocity)*dT; // negative sign means upwards
             velocity += jumpVelocity; // negative sign means upwards
         }
         
 
         // update position
-        scruffyPos.y += velocity;
+        scruffyPos.y += velocity * dT;
         
         // draw rect
         DrawTextureRec(scruffy,scruffyRec,scruffyPos, WHITE);
