b1d58895b220305c7f82f892ebe148bf3b150366
Athul Muralidhar <athul.muralidhar@print.com>
2024-04-22 23:42:31 +0200

    added speed and map movement
    * shits good when everything works out
    * life is good sometimes

--- cpp-fundamentals-game-dev-for-beginners/section4/top-down-2/main.cpp
+++ cpp-fundamentals-game-dev-for-beginners/section4/top-down-2/main.cpp
@@ -1,13 +1,20 @@
 #include "raylib.h"
+#include "raymath.h"
 
-int main() {
+// movement
+// - movement in the game is achieved by moving the map (our map texture) around rather than the character
+
+int main()
+{
     const int windoeWidth = 384;
     const int windowHeight = 384;
+    InitWindow(windoeWidth, windowHeight, "tops");
 
-    InitWindow(windoeWidth, windowHeight,"tops");
-
+    const float speed = 4.0;
 
     Texture2D map = LoadTexture("tileset/map1.png");
+    Vector2 mapPosition{0.0, 0.0};
+    Vector2 direction{0.0, 0.0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -15,15 +22,31 @@ int main() {
         BeginDrawing();
         ClearBackground(WHITE);
 
+        if (IsKeyDown(KEY_A))
+            direction.x -= 1.0;
+        if (IsKeyDown(KEY_D))
+            direction.x += 1.0;
+        if (IsKeyDown(KEY_W))
+            direction.y -= 1.0;
+        if (IsKeyDown(KEY_S))
+            direction.y += 1.0;
 
+        // move the map around
+        if (Vector2Length(direction) != 0.0) // only move the vector if the direction is unchanged
+        {
+            Vector2 normalized = Vector2Normalize(direction); // we want to keep the direction normalized so that there is no extra unwanted movenet
+            // knowthing the direction, we now need to move the map in the opposite direction:
+            // mapPosition = mapPosition - direction
+            // we also need to scale the vector as the usual movement is slow
+            Vector2 scaled = Vector2Scale(normalized, speed);
+            mapPosition = Vector2Subtract(mapPosition, scaled);
+        }
 
-        Vector2 mapPosition{0.0,0.0};
-        DrawTextureEx(map, mapPosition,0.0,4.0, WHITE);
+        DrawTextureEx(map, mapPosition, 0.0, 4.0, WHITE);
 
         EndDrawing();
     }
 
+    UnloadTexture(map);
     CloseWindow();
-    
 }
-
