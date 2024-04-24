b5db6068963e1536c9fcea3f4ea54b0e950cca13
Athul Muralidhar <athul.muralidhar@print.com>
2024-04-24 17:22:48 +0200

    added animation

--- cpp-fundamentals-game-dev-for-beginners/section4/top-down-2/main.cpp
+++ cpp-fundamentals-game-dev-for-beginners/section4/top-down-2/main.cpp
@@ -16,15 +16,20 @@ int main()
     Vector2 mapPosition{0.0, 0.0};
     Vector2 direction{0.0, 0.0};
 
-    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
-    Vector2 knightPosition{
-        .x = windoeWidth / 2.0f - (0.5f * (float)knight.width / 6.0f), // we want the knight to be in the centre of the screen
-        .y = windowHeight / 2.0f - (0.5f * knight.height),
-    };
+    Texture2D knightIdle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D knightRun = LoadTexture("characters/knight_run_spritesheet.png");
+    Texture2D knight{};
+
     const float scaleForKnight = 4.0;
     // 1 == facing right, -1 == facing left
     // this allows us to turn the sprite sheet for the knight so that he faces the appropriate direction
     float rightLeft = 1.f;
+
+    // animation variables
+    float runningTime{};
+    const float updateTime = 1.f / 12.f;
+    int currentFrame{};
+    const int maxFrames = 6;
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -41,8 +46,9 @@ int main()
         if (IsKeyDown(KEY_S))
             direction.y += 1.0;
 
+
         // move the map around
-        if (Vector2Length(direction) != 0.0) // only move the vector if the direction is unchanged
+        if (Vector2Length(direction) != 0.0) // only move the map if the direction is unchanged
         {
             Vector2 normalized = Vector2Normalize(direction); // we want to keep the direction normalized so that there is no extra unwanted movenet
             // knowthing the direction, we now need to move the map in the opposite direction:
@@ -52,14 +58,35 @@ int main()
             mapPosition = Vector2Subtract(mapPosition, scaled);
 
             direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+            // set sprite sheet based on character's movement
+            knight = knightRun;
+        } else {
+            // knight is not moving
+            knight = knightIdle;
         }
 
         // draw the map
         DrawTextureEx(map, mapPosition, 0.0, 4.0, WHITE);
 
+        // update animation frame
+        runningTime += GetFrameTime();
+        if (runningTime >= updateTime)
+        {
+            runningTime = 0.f;
+            currentFrame++;
+
+            if (currentFrame > maxFrames)
+                currentFrame = 0;
+        }
+
         // draw the character
+        Vector2 knightPosition{
+            .x = windoeWidth / 2.0f - (0.5f * (float)knight.width / 6.0f), // we want the knight to be in the centre of the screen
+            .y = windowHeight / 2.0f - (0.5f * knight.height),
+        };
         Rectangle source{
-            .x = 0.0,
+            // for ex: 0 * width of one sprite - this chooses the first sprite
+            .x = currentFrame * (float)knight.width / 6.0f,
             .y = 0.0,
             // if we multiply the width by -1 we get the source texture flipped
             .width = rightLeft * (float)knight.width / 6.0f, // as there are 6 sprites in the sprite sheet
@@ -78,5 +105,7 @@ int main()
     }
 
     UnloadTexture(map);
+    UnloadTexture(knightIdle);
+    UnloadTexture(knightRun);
     CloseWindow();
 }
