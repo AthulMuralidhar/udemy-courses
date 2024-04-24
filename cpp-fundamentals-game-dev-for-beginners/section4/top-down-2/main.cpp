d290dc42c9365bccfe72c389c3a15c24292dc1b5
Athul Muralidhar <athul.muralidhar@print.com>
2024-04-24 16:47:09 +0200

    added direction flipping for knight

--- cpp-fundamentals-game-dev-for-beginners/section4/top-down-2/main.cpp
+++ cpp-fundamentals-game-dev-for-beginners/section4/top-down-2/main.cpp
@@ -19,9 +19,12 @@ int main()
     Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
     Vector2 knightPosition{
         .x = windoeWidth / 2.0f - (0.5f * (float)knight.width / 6.0f), // we want the knight to be in the centre of the screen
-        .y = windowHeight / 2.0f - (0.5 * knight.height),
+        .y = windowHeight / 2.0f - (0.5f * knight.height),
     };
     const float scaleForKnight = 4.0;
+    // 1 == facing right, -1 == facing left
+    // this allows us to turn the sprite sheet for the knight so that he faces the appropriate direction
+    float rightLeft = 1.f;
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -47,6 +50,8 @@ int main()
             // we also need to scale the vector as the usual movement is slow
             Vector2 scaled = Vector2Scale(normalized, speed);
             mapPosition = Vector2Subtract(mapPosition, scaled);
+
+            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
         }
 
         // draw the map
@@ -56,17 +61,18 @@ int main()
         Rectangle source{
             .x = 0.0,
             .y = 0.0,
-            .width = knight.width / 6.0, // as there are 6 sprites in the sprite sheet
-            .height = knight.height,
+            // if we multiply the width by -1 we get the source texture flipped
+            .width = rightLeft * (float)knight.width / 6.0f, // as there are 6 sprites in the sprite sheet
+            .height = (float)knight.height,
         };
 
         Rectangle destination{
             .x = knightPosition.x,
             .y = knightPosition.y,
-            .width = knight.width / 6.0 * scaleForKnight, // as there are 6 sprites in the sprite sheet
+            .width = knight.width / 6.0f * scaleForKnight, // as there are 6 sprites in the sprite sheet
             .height = knight.height * scaleForKnight,
         };
-        DrawTexturePro(knight, source, destination, Vector2{0.0,0.0}, 0.f, WHITE);
+        DrawTexturePro(knight, source, destination, Vector2{0.0, 0.0}, 0.f, WHITE);
 
         EndDrawing();
     }
